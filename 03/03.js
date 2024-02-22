4c776bd74a7f7748a949ac502196d77c3d08d270
baejihyen <baejione@naver.com>
2024-02-22 13:07:57 +0900

    js

--- 03/03.js
+++ 03/03.js
@@ -14,7 +14,17 @@ function handleClick(n) {//js는 함수를 직접 만들수 있다.
 함수선언 2 : 화살표 함수
 상수는 const 변수는 let
 */
+// const handleClick = (n) => {
+//     // let msg ;
+//     // if (n == 1) msg = "안녕하세요,";
+//     // else msg = "안녕히가세요." 
+//     document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다.</h2>` ;
+
+// }
 const handleClick = (n) => {
-    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다.</h2>` ;
+    let msg ;
+    if (n == 1) msg = "<h2>안녕하세요,</h2>";
+    else msg = "<h2>안녕히가세요.</h2>" 
+    document.querySelector("#msgArea").innerHTML = msg;
 
 }
